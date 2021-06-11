9754ada17d3210229a50940ee4c029901795b1a8
amadorm93 <amadorm93@gmail.com>
2021-06-11 00:50:40 -0600

    FormularioRSA primera pagina

---
+++ Presentacion/GestionUsuarios/FormularioRSA.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/PaginasMaestras/MasterDocenteMenu.Master" AutoEventWireup="true" CodeBehind="FormularioRSA.aspx.cs" Inherits="Presentacion.GestionUsuarios.FormularioRSA" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="CphContenedorBase" runat="server">
+     <br />
+    <br />
+    <div class="container">
+     <div class="form-group row">
+         <table>
+             <tr>
+                 <td>
+                       <label class="col-sm-2 col-form-label">Carrera</label>
+                       <asp:TextBox ID="TbCarrera" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox> 
+                 </td>
+                 <td>
+                     <label class="col-sm-2 col-form-label">Semestre</label> 
+                     <asp:TextBox ID="TbSemestre" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox>
+                 </td>
+             </tr>
+             <tr>
+                 <td>
+                       <label class="col-sm-6 col-form-label">Numero de Empleado</label>
+                       <asp:TextBox ID="TbNumEmpleado" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox> 
+                 </td>
+                 <td>
+                     <label class="col-sm-6 col-form-label">Nombre del Docente</label> 
+                     <asp:TextBox ID="TbNombreDocente" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox>
+                 </td>
+             </tr>
+             <tr>
+                  <td>
+                       <label class="col-sm-6 col-form-label">Nombre de Asignatura</label>
+                       <asp:TextBox ID="TbAsignatura" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox> 
+                 </td>
+                 <td>
+                     <label class="col-sm-6 col-form-label">Clave de Asignatura</label> 
+                     <asp:TextBox ID="TbClaveAsig" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox>
+                 </td>
+             </tr>
+             <tr>
+                 <td>
+                     <label class="col-sm-8 col-form-label">¿Cuál es la fecha de la última actualización de los Apuntes docentes (AD)?</label> 
+                     <asp:TextBox ID="TbFecha" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox>
+                     
+                 </td>
+                 <td>
+                      <label class="col-sm-8 col-form-label">¿Proporcionó una copia de la última actualización de los AD al coordinador?</label> 
+                        <asp:DropDownList CssClass="align-self-md-center" ID="DdlAD" runat="server" Width="100px">
+                        <asp:ListItem Text="SI" Value="1" Selected="True"></asp:ListItem>
+                        <asp:ListItem Text="NO" Value="2"></asp:ListItem>
+                        </asp:DropDownList>
+                 </td>
+             </tr>
+              <tr>
+                 <td>
+                     <label class="col-sm-8 col-form-label">En su caso. ¿Cuál es la fecha de la última actualización del Manual de Prácticas (MP)?</label> 
+                     <asp:TextBox ID="TbFecha2" runat="server" CssClass="form-control box w-100" Width="700px"></asp:TextBox>
+                     
+                 </td>
+                 <td>
+                      <label class="col-sm-8 col-form-label">Si aplica, ¿Proporcionó una copia de la última actualización del MP al coordinador?</label> 
+                        <asp:DropDownList CssClass="align-self-md-center" ID="DdlMP" runat="server"  Width="100px">
+                        <asp:ListItem Text="SI" Value="1" Selected="True"></asp:ListItem>
+                        <asp:ListItem Text="NO" Value="2"></asp:ListItem>
+                        </asp:DropDownList>
+                 </td>
+             </tr>
+             <tr>
+                 <td>
+                     <label class="col-sm-8 col-form-label">¿Cuántos semestres ha impartido esta materia?</label>
+                     <asp:TextBox ID="tbSemestresImp" runat="server" CssClass="form-control box w-100" Width="150px" ></asp:TextBox><br />
+                 </td>
+             </tr>
+         </table>
+         <div class="form-row align-content-center text-center">
+              <div class="col">
+                <asp:Button runat="server" ID="btnRegresar" CssClass="btn btn-light boton mb-2 box margen"  Text="Regresar"></asp:Button>
+
+            </div>
+            <div class="col">
+                 <asp:Button runat="server" ID="btnSiguiente" CssClass="btn btn-light boton mb-2 box margen"   Text="Siguiente Pagina"></asp:Button>
+            </div>
+         </div>
+        </div>
+        
+</asp:Content>
---
+++ Presentacion/GestionUsuarios/FormularioRSA.aspx.designer.cs
@@ -0,0 +1,134 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentacion.GestionUsuarios
+{
+
+
+    public partial class FormularioRSA
+    {
+
+        /// <summary>
+        /// Control TbCarrera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbCarrera;
+
+        /// <summary>
+        /// Control TbSemestre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbSemestre;
+
+        /// <summary>
+        /// Control TbNumEmpleado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbNumEmpleado;
+
+        /// <summary>
+        /// Control TbNombreDocente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbNombreDocente;
+
+        /// <summary>
+        /// Control TbAsignatura.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbAsignatura;
+
+        /// <summary>
+        /// Control TbClaveAsig.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbClaveAsig;
+
+        /// <summary>
+        /// Control TbFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbFecha;
+
+        /// <summary>
+        /// Control DdlAD.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlAD;
+
+        /// <summary>
+        /// Control TbFecha2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TbFecha2;
+
+        /// <summary>
+        /// Control DdlMP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlMP;
+
+        /// <summary>
+        /// Control tbSemestresImp.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbSemestresImp;
+
+        /// <summary>
+        /// Control btnRegresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegresar;
+
+        /// <summary>
+        /// Control btnSiguiente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSiguiente;
+    }
+}
